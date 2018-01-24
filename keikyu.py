261e18a99b508957a02252a8e1a3d3358e8ec625
pokio <32353873+pokiiio@users.noreply.github.com>
2018-01-24 11:41:54 +0900

    Add files via upload

---
+++ keikyu.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+
+import urllib2
+from HTMLParser import HTMLParser
+
+def get_unko():
+    # 京急運行情報のパーサー
+    class KeikyuUnkoParser(HTMLParser):
+
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.flag = False
+            self.unko = ""
+
+        # divタグで、classが"unko-panel"なものを探す
+        def handle_starttag(self, tag, attrs):
+            attrs = dict(attrs)
+            if tag == "div" and "class" in attrs and attrs["class"] == "unko-panel":
+                self.flag = True
+
+        # 上記の条件に合致するタグが発見できたら、そのタグで囲まれているdataを読み取る
+        def handle_data(self, data):
+            if self.flag:
+                self.unko = self.unko + data
+
+        # divタグ終了時に読み取り終了
+        def handle_endtag(self, tag):
+            self.flag = False
+
+        # 読み取った運行情報を整形して返す
+        def get_unko(self):
+            return self.unko.replace("\n", "").replace("\r", "").decode("utf-8")
+
+    # 京急の運行情報ページ
+    url = "http://unkou.keikyu.co.jp/"
+
+    # 運行情報ページを取得
+    response = urllib2.urlopen(url)
+
+    # パーサーに食わせる
+    parser = KeikyuUnkoParser()
+    parser.feed(response.read())
+
+    # 諸々close()
+    parser.close()
+    response.close()
+
+    # 運行情報を返す
+    return parser.get_unko()
